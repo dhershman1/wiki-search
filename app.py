79999d739abf44dc65bf65a3684386438e3f8ade
dhershman1 <dustinh17@gmail.com>
2021-08-09 22:57:06 -0400

    Added exception handler

--- app.py
+++ app.py
@@ -5,19 +5,27 @@ app = Flask(__name__)
 app.config['SERVER_NAME'] = 'localhost:5000'
 
 def build_url(str):
-  return "https://en.wikipedia.org/wiki/{}".format(str.replace(' ', '_'))
+  return 'https://en.wikipedia.org/wiki/{}'.format(str.replace('"', '').replace(' ', '_'))
 
-@app.route('/', subdomain="<search>", methods=["GET"])
+def contains_search(search):
+  return lambda str: search.lower() in str.lower()
+
+@app.route('/', subdomain='<search>', methods=['GET'])
 def search_index(search):
-  page = wikipedia.search(search, 15)
-  if page[0].lower() in search.lower():
-    return {
-      "links": [build_url(page[0])]
-    }
+  try:
+    wikipedia.page(search)
 
-  return {
-    "links": list(map(build_url, page))
-  }
+    return {
+      'links': [build_url(search)]
+    }
+  except wikipedia.DisambiguationError as e:
+    filtered_list = list(filter(contains_search(search), e.options))
+
+    return {
+      'links': list(map(build_url, filtered_list))
+    }
+  except:
+    return "Something terrible has happened!"
 
 @app.route('/')
 def hello_world():
